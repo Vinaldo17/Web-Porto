14f8bb471ff69eff698b0ef5dd8c5bafaaed66dd
Vinaldo17 <117942185+Vinaldo17@users.noreply.github.com>
2024-10-22 11:29:16 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-document.addEventListener('scroll', function() {
-    const navbar = document.getElementById('navbar');
-    if (window.scrollY > 110) {
-      navbar.classList.add('scrolled');
-    } else {
-      navbar.classList.remove('scrolled');
-    }
-  });
 
 window.addEventListener('scroll',() => {
     const image = document.getElementById('darken-image');
@@ -14,10 +6,23 @@ window.addEventListener('scroll',() => {
 
     // Calculate darkness level based on scroll position (between 0 and 1)
     const darknessLevel = Math.min(scrollTop / maxScroll, 1);
-    image.style.filter = `brightness(${1 - 4*(darknessLevel)})`;
+    image.style.filter = `brightness(${1 - 3*(darknessLevel)})`;
     // Apply the darkness as a CSS filter (darker as you scroll more)
     
 });
+
+window.addEventListener('scroll', function() {
+  const navbar = document.querySelector('.navbar');
+  if (window.scrollY > 50) {
+      
+      navbar.classList.add('scrolled');
+  } else {
+      navbar.classList.remove('scrolled');
+      
+  }
+});
+
+
 
 const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry)=>{
@@ -31,7 +36,7 @@ const observer = new IntersectionObserver((entries) => {
   });
 });
 
-const hiddenElements = document.querySelectorAll('.hidden',);
+const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el)=> observer.observe(el));
 
 
@@ -47,5 +52,21 @@ const observery = new IntersectionObserver((entries) => {
   });
 });
 
+
 const hiddenElementsy = document.querySelectorAll('.hiddeny');
 hiddenElementsy.forEach((el)=> observer.observe(el));
+
+const observeryx = new IntersectionObserver((entries) => {
+  entries.forEach((entry)=>{
+  console.log(entry)
+  if(entry.isIntersecting){
+    entry.target.classList.add('showx');
+  }else{
+    entry.target.classList.remove('showx');
+  }
+  
+  });
+});
+
+const hiddenElementsx = document.querySelectorAll('.hiddenx',);
+hiddenElementsx.forEach((el)=> observer.observe(el));
